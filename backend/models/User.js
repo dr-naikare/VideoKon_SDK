20b64ca14f9bd4f99c5bedfc37bf91ea9b42597e
dr-naikare <naikaresuyash@gmail.com>
2024-08-17 10:13:08 +0530

    Solved Errors
    Solved Login Error Which was caused by double hashing of the password which used to create problem while authentication
    
    Also solved indexing problem which was stoping from registering users
    
    Co-Authored-By: Mustafa Bhikhapurwala <mustafabhikhpurwala@gmail.com>

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true }
 });
 
 // Hash password before saving user
