409666e074f18c4c8c5125227d08e7032ac20ba2
dr-naikare <naikaresuyash@gmail.com>
2024-09-22 11:15:21 +0530

    Improved Auth
    - add cookie utility

--- backend/controllers/fetchUser.js
+++ backend/controllers/fetchUser.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const fetchUser = async (req, res) => {
     try {
-        const user = await User.findById(req.user).select('-password');
+        const user = await User.findById(req.headers.userId).select('-password');
         res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ message: 'Server error' });
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,16 +4,15 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const authMiddleware = (req, res, next) => {
-    const authheader = req.header('Authorization');
-    const token = authheader.split(' ')[1];
+    const authHeader = req.headers.authorization;
+    if(!authHeader) return res.status(401).json({ message: 'No token, authorization denied' });
+    const token = authHeader.split(' ')[1];
 
-    if (!token) {
-        return res.status(401).json({ message: 'No token, authorization denied' });
-    }
+    if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.id;
+        req.headers.userId = decoded.id;
         next();
     } catch (error) {
         res.status(401).json({ message: 'Token is not valid' });
