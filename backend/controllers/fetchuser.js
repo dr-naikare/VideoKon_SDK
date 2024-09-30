0be90c390121c96060c9e0502a800dc84932330c
Mustafa <mustafabhikhpurwala@gmail.com>
2024-09-30 14:02:08 +0530

    fix fetch user

--- backend/controllers/fetchuser.js
+++ backend/controllers/fetchuser.js
@@ -1,21 +1,19 @@
+const { request } = require('express');
 const User = require('../models/User');
-const dotenv = require('dotenv');
 
+const dotenv = require('dotenv');
 dotenv.config();
 
-const fetchUser = async (req, res) => {
-    // Use req.userId instead of request.headers.userId
-    const userId = req.userId; 
-    console.log(userId);
-
+const fetchUser =  async (req, res) => {
+    const userId = request.userId;
+    console.log('userId:', userId);
     try {
-        const user = await User.findById(userId).select('-password'); 
+        const user = await User.findById(req.headers.userId).select('-password'); // Exclude the password field
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
         res.json(user);
     } catch (error) {
-        console.error('Error fetching user:', error); 
         res.status(500).json({ message: 'Server error' });
     }
 };
