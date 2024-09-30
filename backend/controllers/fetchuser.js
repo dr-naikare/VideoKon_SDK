0173e2af728dc3eab67ad11a47d2960b1680ca59
Mustafa <mustafabhikhpurwala@gmail.com>
2024-09-30 13:59:50 +0530

    fetch user fix

--- backend/controllers/fetchuser.js
+++ backend/controllers/fetchuser.js
@@ -5,17 +5,17 @@ dotenv.config();
 
 const fetchUser = async (req, res) => {
     // Use req.userId instead of request.headers.userId
-    const userId = req.userId; // This should be set in your authMiddleware
+    const userId = req.userId; 
     console.log(userId);
 
     try {
-        const user = await User.findById(userId).select('-password'); // Exclude the password field
+        const user = await User.findById(userId).select('-password'); 
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
         res.json(user);
     } catch (error) {
-        console.error('Error fetching user:', error); // Log the error for debugging
+        console.error('Error fetching user:', error); 
         res.status(500).json({ message: 'Server error' });
     }
 };
